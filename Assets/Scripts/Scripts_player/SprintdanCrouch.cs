7745565b6eb74b929377cbf40fd91d2fe16cfff9
gnotnek <56432757+gnotnek@users.noreply.github.com>
2020-10-14 20:59:55 +0700

    mbenakke jeneng file ro nambahi sprint ro ndodok

---
+++ Assets/Scripts/Scripts_player/SprintdanCrouch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprintdanCrouch : MonoBehaviour {
+    // Start is called before the first frame update
+     private PlayerMovement playerMovement;
+    public float kecepatan_lari = 10f;
+    public float kecepatan_bergerak = 5f;
+    public float merayap = 2f;
+
+    private Transform look_Root;
+    private float tinggi_berdiri = 1.6f;
+    private float tinggi_merangkak = 1f;
+
+    private bool sedang_merangkak;
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        look_Root = transform.GetChild(0);
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     sprint(); 
+     merangkak();  
+    }
+    
+
+    //mlayu
+    void sprint(){
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !sedang_merangkak){
+            playerMovement.speed = kecepatan_lari;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift) && !sedang_merangkak){
+            playerMovement.speed = kecepatan_bergerak;
+        }
+    }
+
+    //merangkak
+    void merangkak(){
+        if(Input.GetKeyDown(KeyCode.C)){
+            if(sedang_merangkak){
+                look_Root.localPosition = new Vector3(0f, tinggi_berdiri, 0f);
+                playerMovement.speed = kecepatan_bergerak;
+
+                sedang_merangkak = false;
+            }else{
+                look_Root.localPosition = new Vector3(0f, tinggi_merangkak, 0f);
+                playerMovement.speed = merayap;
+
+                sedang_merangkak = true;
+            }
+        }
+
+    }
+} 
