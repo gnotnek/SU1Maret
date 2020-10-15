01be256f6c228ffec329618e6cf3f59a34e24a28
gnotnek <56432757+gnotnek@users.noreply.github.com>
2020-10-15 16:12:55 +0700

    menambahkan sound tapi belum muncul suara ne, jadi aku leren sek

---
+++ Assets/Scripts/Scripts_player/PlayerFootsteps.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerFootsteps : MonoBehaviour {
+
+    private AudioSource footsteps_sound;
+
+    [SerializeField]
+    private AudioClip[] footstepClips;
+
+    private CharacterController character_Controller;
+
+    [HideInInspector]
+    public float volume_Min, volume_Max;
+
+    private float accumulated_Distance;
+
+    [HideInInspector]
+    public float step_Distance;
+
+    void Awake(){
+        footsteps_sound = GetComponent<AudioSource>();
+
+        character_Controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        CheckToPlayFootstepSound();
+    }
+
+    void CheckToPlayFootstepSound(){
+        if(!character_Controller.isGrounded)
+        return;
+      
+        if(character_Controller.velocity.sqrMagnitude > 0){
+            //akumulasi jarak paling jauh kyta jalan
+            //sampai kita memaikan foosteps sound
+            accumulated_Distance += Time.deltaTime;
+
+            if(accumulated_Distance>step_Distance){
+                footsteps_sound.volume = Random.Range(volume_Min, volume_Max);
+                footsteps_sound.clip = footstepClips[Random.Range(0, footstepClips.Length)];
+                footsteps_sound.Play();
+
+                accumulated_Distance = 0f;
+            }
+        }else {
+            accumulated_Distance = 0f;
+        }
+    }
+}
--- Assets/Scripts/Scripts_player/SprintdanCrouch.cs
+++ Assets/Scripts/Scripts_player/SprintdanCrouch.cs
@@ -11,15 +11,32 @@ public class SprintdanCrouch : MonoBehaviour {
 
     private Transform look_Root;
     private float tinggi_berdiri = 1.6f;
-    private float tinggi_merangkak = 1f;
+    private float tinggi_merangkak = 0.6f;
 
     private bool sedang_merangkak;
+
+    private PlayerFootsteps player_footsteps;
+    private float sprint_Volume = 1f;
+    private float merangkak_volume = 0.1f;
+    private float walk_vol_min = 0.2f;
+    private float walk_vol_max = 0.6f;
+
+    private float walk_step_distance = 0.4f;
+    private float sprint_step_distance = 0.25f;
+    private float Crouch_step_distance = 0.5f;
     void Awake()
     {
         playerMovement = GetComponent<PlayerMovement>();
         look_Root = transform.GetChild(0);
 
+        player_footsteps = GetComponentInChildren<PlayerFootsteps>();
 
+    }
+
+    void Start() {
+        player_footsteps.volume_Min = walk_vol_min;
+        player_footsteps.volume_Max = walk_vol_max;
+        player_footsteps.step_Distance = walk_step_distance;
     }
     // Update is called once per frame
     void Update()
@@ -33,10 +50,18 @@ public class SprintdanCrouch : MonoBehaviour {
     void sprint(){
         if(Input.GetKeyDown(KeyCode.LeftShift) && !sedang_merangkak){
             playerMovement.speed = kecepatan_lari;
+
+            player_footsteps.step_Distance = sprint_step_distance;
+            player_footsteps.volume_Min = sprint_Volume;
+            player_footsteps.volume_Max = sprint_Volume;
         }
 
         if(Input.GetKeyUp(KeyCode.LeftShift) && !sedang_merangkak){
             playerMovement.speed = kecepatan_bergerak;
+
+            player_footsteps.volume_Min = walk_vol_min;
+            player_footsteps.volume_Max = walk_vol_max;
+            player_footsteps.step_Distance = walk_step_distance;
         }
     }
 
@@ -44,13 +69,23 @@ public class SprintdanCrouch : MonoBehaviour {
     void merangkak(){
         if(Input.GetKeyDown(KeyCode.C)){
             if(sedang_merangkak){
+                //sedang berdiri
                 look_Root.localPosition = new Vector3(0f, tinggi_berdiri, 0f);
                 playerMovement.speed = kecepatan_bergerak;
 
+                player_footsteps.volume_Min = walk_vol_min;
+                player_footsteps.volume_Max = walk_vol_max;
+                player_footsteps.step_Distance = walk_step_distance;
+
                 sedang_merangkak = false;
             }else{
+                //sedang merangkak
                 look_Root.localPosition = new Vector3(0f, tinggi_merangkak, 0f);
                 playerMovement.speed = merayap;
+
+                player_footsteps.step_Distance = Crouch_step_distance;
+                player_footsteps.volume_Min = merangkak_volume;
+                player_footsteps.volume_Max = merangkak_volume;
 
                 sedang_merangkak = true;
             }
