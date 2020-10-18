cd2461274ebb32043f1658df307f2a7c6248258e
gnotnek <56432757+gnotnek@users.noreply.github.com>
2020-10-18 18:29:36 +0700

    Nambah script game, durung sido tak tulis selak ngisi wawancara

---
+++ Assets/Scripts/Weapon Scripts/WeaponHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim{
+    NONE,
+    SELF_AIM,
+    AIM
+    
+}
+
+public enum WeaponFireType{
+    SINGLE,
+    MULTIPLE
+}
+
+publin enum WeaoponBulletType{
+    BULLET,
+    SPEAR,
+    NONE
+}
+public class WeaponHandler : MonoBehaviour{
+
+    private animator anim;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
